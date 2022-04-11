3f70a919e8293ade950618444c645335b2111613
Chris Rundell <cdrundell@icloud.com>
2022-04-11 16:54:40 +0100

    incomplete as need API key

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,17 +1,45 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 import FlatList from './flat_list';
-import SimpleMap from './map';
+import flats from './flats.js'
+import Marker from './marker.jsx'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedFlat: flats[0],
+      flats
+    };
+  }
+
+  selectFlat = (index) => {
+    this.setState({ selectedFlat: flats[index] });
+  }
+
+  center() {
+    return {
+      lat: this.state.selectedFlat.lat,
+      lng: this.state.selectedFlat.lng
+    };
+  }
+
+
   render () {
     return (
       <div>
-          <FlatList />
+        <FlatList
+          flats={this.state.flats}
+          selectedFlat={this.state.selectedFlat}
+          selectFlat={this.selectFlat}
+        />
         <div className="map-container">
-          <SimpleMap />
+          <GoogleMapReact defaultCenter={this.center()} defaultZoom={12}>
+            <Marker lat={this.state.selectedFlat.lat} lng={this.state.selectedFlat.lng} />
+          </GoogleMapReact>
         </div>
       </div>
-      )
+    );
   }
 }
 
