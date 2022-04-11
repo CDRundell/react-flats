7710baf68f3afa77492e750bf2a6451d1c1e1472
Chris Rundell <cdrundell@icloud.com>
2022-04-11 13:58:42 +0100

    google maps added

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import FlatList from './flat_list';
+import SimpleMap from './map';
 
 class App extends Component {
   render () {
     return (
       <div>
-        <div className="flat-list">
           <FlatList />
-        </div>
         <div className="map-container">
-          <h1> HELLO WORLD </h1>
+          <SimpleMap />
         </div>
       </div>
       )
--- src/components/flat_list.jsx
+++ src/components/flat_list.jsx
@@ -5,12 +5,14 @@ class FlatList extends Component {
   render () {
     return (
       <div className="flat-list">
-        {flats.map((flat) => {
+        {flats.map((flat, index) => {
 
-          return (<div className="card" style={{backgroundImage: `url(${flat.imageUrl})`}}>
+          return (<div className="card" style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url(${flat.imageUrl})`}}>
+            <div class="card-category">{flat.price} {flat.priceCurrency}</div>
             <div className="card-description">
               <h2> {flat.name} </h2>
             </div>
+            <a class="card-link" href="#"></a>
           </div>
           )
         })
